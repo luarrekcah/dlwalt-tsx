1c1e8ffd213d9e6fadd14fc1a32a6d3cb829011e
Raul Rodrigues <64855653+luarrekcah@users.noreply.github.com>
2025-11-21 14:55:54 -0500

    pixerl

--- src/pages/components/HelmetHeader.tsx
+++ src/pages/components/HelmetHeader.tsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet";
 
-const HelmetHeader = ({title = 'Página Principal', description= 'Seja bem vindo a D | Walt!', url = '', image = '/images/ogimages/index.jpg'}) => {
+const HelmetHeader = ({ title = 'Página Principal', description = 'Seja bem vindo a D | Walt!', url = '', image = '/images/ogimages/index.jpg' }) => {
   return (
     <Helmet>
       <title>{title} - D | Walt Engenharia</title>
@@ -33,6 +33,21 @@ empresas de energia solar"
         content={`${description}`}
       />
       <meta name="twitter:image" content={image} />
+      <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '25094130833530947');
+        fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=25094130833530947&ev=PageView&noscript=1"
+      /></noscript>
     </Helmet>
   );
 };
