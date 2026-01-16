6e4c58800e66e8a4eb147cef2f1714b375904276
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2026-01-15 22:14:23 -0300

    ajuste dos dados

--- lib/data/company.ts
+++ lib/data/company.ts
@@ -4,40 +4,39 @@ export const COMPANY_INFO = {
     cnpj: "12.345.678/0001-99",
     description: "Especialistas em energia solar fotovoltaica e eficiência energética.",
     contact: {
-        phone: "(11) 99999-9999",
-        whatsapp: "5511999999999",
-        email: "contato@dwaltenergia.com.br",
+        phone: "(69) 99369-5702",
+        whatsapp: "5569993695702",
+        email: "contato@dwalt.net",
     },
     social: {
-        instagram: "https://instagram.com/dwaltenergia",
-        facebook: "https://facebook.com/dwaltenergia",
-        linkedin: "https://linkedin.com/company/dwaltenergia",
-        youtube: "https://youtube.com/dwaltenergia",
+        instagram: "https://www.instagram.com/dwalt_energia",
+        facebook: "https://www.facebook.com/Dwaltenergiaoficial",
+        youtube: "https://www.youtube.com/@Dwalt_energia",
     },
     units: [
         {
             id: "matriz",
-            name: "Matriz - São Paulo",
-            address: "Av. Paulista, 1000 - Bela Vista, São Paulo - SP, 01310-100",
-            phone: "(11) 3333-3333",
-            whatsapp: "5511933333333",
-            mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.197586615555!2d-46.65432688502227!3d-23.56396838468164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa365%3A0x4c26b2b3b80894be!2sAv.%20Paulista%2C%201000%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-100!5e0!3m2!1spt-BR!2sbr!4v1675367890123!5m2!1spt-BR!2sbr"
+            name: "Matriz - Ariquemes",
+            address: "Av. Canaã, 2500 - St. 03, Ariquemes - RO, 76870-164",
+            phone: "(69) 99369-5702",
+            whatsapp: "5569993695702",
+            mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7860.486182628491!2d-63.04342!3d-9.913701!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93cc90c1e2c420cf%3A0xfd5a1fbeea15df07!2sDWALT%20Energia%20Solar%20%7C%20Ariquemes%20%E2%80%93%20RO!5e0!3m2!1spt-BR!2sbr!4v1768525844958!5m2!1spt-BR!2sbr"
         },
         {
-            id: "campinas",
-            name: "Unidade Campinas",
-            address: "R. Barão de Jaguara, 500 - Centro, Campinas - SP, 13015-001",
-            phone: "(19) 3333-4444",
-            whatsapp: "5519933334444",
-            mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3674.8078657682613!2d-47.06059288503884!3d-22.9099383850123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c61705555505%3A0x4c26b2b3b80894be!2sR.%20Bar%C3%A3o%20de%20Jaguara%2C%20500%20-%20Centro%2C%20Campinas%20-%20SP%2C%2013015-001!5e0!3m2!1spt-BR!2sbr!4v1675367890123!5m2!1spt-BR!2sbr"
+            id: "porto-velho",
+            name: "Unidade Porto Velho",
+            address: "R. Mte. Gabriel, 5368 - Flodoaldo Pontes Pinto, Porto Velho - RO, 76820-620",
+            phone: "(69) 99369-5702",
+            whatsapp: "5569993695702",
+            mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4037098.6606353167!2d-64.235953!3d-8.834901!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x92324314cbdbec11%3A0xd3f63a28d0042aaf!2sDWALT%20ENERGIA%20SOLAR%20%7C%20Porto%20Velho%20%E2%80%93%20RO!5e0!3m2!1spt-BR!2sbr!4v1768526010392!5m2!1spt-BR!2sbr"
         },
         {
-            id: "ribeirao",
-            name: "Unidade Ribeirão Preto",
-            address: "Av. Pres. Vargas, 1200 - Jardim América, Ribeirão Preto - SP, 14020-260",
-            phone: "(16) 3333-5555",
-            whatsapp: "5516933335555",
-            mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.597586615555!2d-47.81432688502227!3d-21.18396838468164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b9bf8c8da0aa36%3A0x4c26b2b3b80894be!2sAv.%20Pres.%20Vargas%2C%201200%20-%20Jardim%20Am%C3%A9rica%2C%20Ribeir%C3%A3o%20Preto%20-%20SP%2C%2014020-260!5e0!3m2!1spt-BR!2sbr!4v1675367890123!5m2!1spt-BR!2sbr"
+            id: "rolim-de-moura",
+            name: "Unidade Rolim de Moura",
+            address: "Av. 25 de Agosto - Centro, Rolim de Moura - RO, 76940-000",
+            phone: "(69) 99369-5702",
+            whatsapp: "5569993695702",
+            mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15626.342431624877!2d-61.768122000000005!3d-11.723735!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93c86b15415371a5%3A0x12d9867ca24b5768!2sDWALT%20ENERGIA%20SOLAR%20%7C%20Rolim%20de%20Moura%20%E2%80%93%20RO!5e0!3m2!1spt-BR!2sbr!4v1768525994578!5m2!1spt-BR!2sbr"
         }
     ]
 };
